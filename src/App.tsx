a6a044fc565afe10786daa601bc0d709cc31657a
Eduardo Marques <programdor.eduardo@gmail.com>
2022-10-30 21:12:09 -0300

    first section

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//components
+import Footer from './components/Footer';
+import Header from './components/Header';
+//CSS
+import styles from './App.module.css'
+
+function App() {
+  return <div>
+    <Header />
+    <main className={styles.main}>
+      <h1>Conteúdo...</h1>
+    </main>
+    <Footer />
+  </div>
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './Header.module.css'
+
+
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+        <h1>React com TypeScript</h1>
+    </header>
+  )
+}
+
+export default Header
